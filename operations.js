5a5b1dd7c199ad03b73bc506a5f81c4e9c65b41f
Minchao Duclos <chaohg@hotmail.com>
2021-06-24 14:54:59 -0700

    change callback to promises

--- operations.js
+++ operations.js
@@ -1,34 +1,22 @@
-// this version is to use module to organize the operations.
-const assert = require('assert').strict;
+// this version is to change the callback hell to use promises
 
-exports.insertDocument = (db, document, collection, callback) => {
-    const coll = db.collection(collection); // this is to save campsites the collection name to the coll
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);  // this callback is written in index.js
-    });
-};
-
-exports.findDocuments = (db, collection, callback) => {
+exports.insertDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.insertOne(document);
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.find({}).toArray();
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.updateOne(document, { $set: update }, null, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
+
+exports.updateDocument = (db, document, update, collection) => {
+    const coll = db.collection(collection);
+    return coll.updateOne(document, { $set: update }, null);
+};
+
