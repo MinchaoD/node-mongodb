8b96852d4ebb018522d68dfffe66902f4a27e09f
Minchao Duclos <chaohg@hotmail.com>
2021-06-24 00:22:57 -0700

    node-mongodb example 2

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert').strict;
+const dboper = require('./operations');
 
-const url = 'mongodb://localhost:27017/';
+const url = 'mongodb://localhost:27017/'; // the default mongodb server location
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, {useUnifiedTopology: true}, (err, client)=>{
-    assert.strictEqual(err,null);
+MongoClient.connect(url, {useUnifiedTopology: true}, (err, client)=>{  // connect to the mongodb server. useUnifiedTopology is for avoid a default error
+    assert.strictEqual(err,null); // if err is strict equal to null, then everything is fine and we can continnue to the next console.log.
+    //  If is err is not equal to null, then it will quit
     console.log('Connected correctly to server');
     const db = client.db(dbname);
-    db.dropCollection('campsites', (err, result) =>{
+    db.dropCollection('campsites', (err, result) =>{  // this is to drop/delete the campsites collection, so we have a clean data and will recreate it later
         assert.strictEqual(err, null);
         console.log("Dropped Collection", result);
 
-        const collection = db.collection('campsites');
+        dboper.insertDocument(db, { name: "Breadcrumb Trail Campground", description: "Test"},
+        'campsites', result =>{
+            console.log('Insert Documents', result.ops); // this is the defining function, will be called later
+                dboper.findDocuments(db, 'campsites', docs =>{
+                    console.log('Found Documents:', docs);
+                    dboper.updateDocument(db, { name: "Breadcrumb Trail Campground" },  // here name:  is the document needs to be updated, the description here is the updated replaced new version
+                    { description: "Updated Test Description" }, 'campsites', result=>{
+                        console.log('Updated Document Count:', result.result.nModified);
+                        dboper.findDocuments(db, 'campsites', docs=>{
+                            console.log('Found Documents:', docs);
+                            dboper.removeDocument(db, { name: "Breadcrumb Trail Campground" },
+                            'campsites', result =>{
+                                console.log('Deleted Document Count:', result.deletedCount);
+                                client.close()
+                            })
+                        })
 
-        collection.insertOne({name: "Breadcrumb Trail Campground", description: "test"},
-        (err, result) =>{
-            assert.strictEqual(err, null);
-            console.log("Insert Document:", result.ops);
-            collection.find().toArray((err, docs) =>{
-                assert.strictEqual(err, null);
-                console.log('Found Documents:', docs);
-                client.close()
-            })
+                    })
+                })
         })
+
+      
     })
 })
---
+++ operations.js
@@ -0,0 +1,33 @@
+const assert = require('assert').strict;
+
+exports.insertDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection); // this is to save campsites the collection name to the coll
+    coll.insertOne(document, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);  // this callback is written in index.js
+    });
+};
+
+exports.findDocuments = (db, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.find().toArray((err, docs) => {
+        assert.strictEqual(err, null);
+        callback(docs);
+    });
+};
+
+exports.removeDocument = (db, document, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.deleteOne(document, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);
+    });
+};
+
+exports.updateDocument = (db, document, update, collection, callback) => {
+    const coll = db.collection(collection);
+    coll.updateOne(document, { $set: update }, null, (err, result) => {
+        assert.strictEqual(err, null);
+        callback(result);
+    });
+};
